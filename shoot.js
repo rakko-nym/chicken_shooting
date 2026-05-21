189bd2afe619100f6ab29478b44db559c5403ce4
rakko-nym <lioncatyama110@yahoo.co.jp>
2026-05-21 22:14:10 +0900

    chicken boss

--- shoot.js
+++ shoot.js
@@ -8,7 +8,7 @@ ctx.imageSmoothingEnabled = false;
 const player = {
     playerX : canvas.width / 2 , 
     playerY : canvas.height /2 , 
-    playerSize : 50 , 
+    playerSize : 30 , 
     playerSpeed : 5 , 
     playerHP : 100 , 
     playerInvincible : false ,
@@ -29,12 +29,13 @@ let pShots = [];
 let Enemies = [];
 const pShotSize = 10 ;
 const pShotSpeed = 5 ;
-const EnemySpeed = 3 ;
 const EnemySize = 35 ;
+let bossAngle = 0 ;
 let EnemySpawnQueue = 2000 ;
 const EnemyType = ['penguin','parrot','toucan'];
 let lastSpawnTime = 0 ;
 let Muki = 0;
+
 
 addEventListener('keyup',(event)=>{keys[event.key] = false ;})
 addEventListener('keydown',(event)=>{keys[event.key] = true ;})
@@ -48,7 +49,7 @@ ctx.clearRect(0,0,canvas.width,canvas.height);
 playerMove(time);
 pShotMove();
 EnemySpawn(time);
-EnemyMove();
+EnemyMove(time);
 playerEnemyHit(time);
 pShotEnemyHit();
 
@@ -58,7 +59,7 @@ window.requestAnimationFrame(gameLoop);
 // デバッグ
 ctx.fillStyle = "white";
 ctx.font = "20px Arial";
-ctx.fillText("HP: " + player.playerHP, 20, 30);
+ctx.fillText("HP: " + Math.floor(player.playerHP), 20, 30);
 ctx.fillText(player.isReload,20,50) ;
 };
 
@@ -78,10 +79,10 @@ if(mouseX - player.playerX > 0){
     else{
     ctx.scale(-1,1);
     }
-        if(player.ammo <= 0){
-            if(!player.isReload){player.lastReloadTime = currentTime ;}
-            player.isReload = true ;
-        }
+        if(player.ammo <= 0 && !player.isReload){
+            player.lastReloadTime = currentTime;
+            player.isReload = true;
+         }
         if(player.isReload && currentTime - player.lastReloadTime > player.reloadTime){
             player.isReload = false ;
             player.ammo = player.maxAmmo ;
@@ -96,7 +97,7 @@ if(keys.s){player.playerY += player.playerSpeed;};
 if(keys.d){player.playerX += player.playerSpeed;};
 if(keys.a){player.playerX -= player.playerSpeed;};
 if(keys.click && !player.isReload){
-    pShots.push(new playerShot(player.playerX+player.playerSize/2, player.playerY+player.playerSize/2, pShotSize));
+    pShots.push(new playerShot(player.playerX+player.playerSize/2, player.playerY+player.playerSize/2));
     keys.click = false ;
     drawImg(10 , -player.playerSize / 2, -player.playerSize / 2, player.playerSize);}else{
     drawImg((Math.floor(frameCounter / 10) % 3) + 7, -player.playerSize / 2, -player.playerSize / 2, player.playerSize);
@@ -104,7 +105,7 @@ if(keys.click && !player.isReload){
     ctx.restore();
 }
 class playerShot {
-    constructor(pShotX,pShotY,pShotSize){
+    constructor(pShotX,pShotY){
         this.pShotX = pShotX ;
         this.pShotY = pShotY ;
         this.pShotSize = pShotSize
@@ -145,35 +146,73 @@ function pShotMove(){
 }
 
 class Enemy {
-    constructor(EnemyX,EnemyY,EnemySpeed,EnemyHP,type,EnemySize){
+    constructor(EnemyX,EnemyY,EnemyHP,type,EnemySize){
         this.type = type ;
         this.EnemyX = EnemyX ;
         this.EnemyY = EnemyY ;
-        this.EnemySpeed = EnemySpeed ;
         this.EnemyHP = EnemyHP ;
         this.EnemyPower = Math.floor(Math.random()*3) + 3 ;
         this.EnemyDx = 0 ;
         this.EnemyDy = 0 ;
         this.EnemyAngle = 0 ;
         this.active = true ;
+        this.EnemySpeed = 0 ;
         this.EnemySize = EnemySize
+    switch(this.type){
+        case 'penguin' :
+             this.EnemyPower = 3 ;
+             this.EnemySpeed = 4 ;
+            break;
+        case 'parrot' : 
+            this.EnemyPower = 5 ;
+            this.EnemySpeed = 2.5 ;
+            break;
+        case 'toucan' :
+            this.EnemyPower = 8 ;
+            this.EnemySpeed = 1.5 ;
+            break;
+        case 'boss'   :
+            this.EnemySpeed = 0.5 ;
+            this.EnemyHP = 30 ;
+            this.EnemySize = 300 ;
+            this.bossAngle = 0 ;
+            this.foo = 1 ;
+            this.lastShotTime = 0 ;
+            break;
+        case 'shot'   :
+             this.EnemySpeed = 3 ;
+             this.EnemyVx = Math.cos(bossAngle) * this.EnemySpeed ;
+             this.EnemyVy = Math.sin(bossAngle) * this.EnemySpeed ;
+            break;
+             }
 
     }
-    move (){
+    move (currentTime){
+        if(this.type !== 'shot'){
          this.EnemyDx = player.playerX - this.EnemyX ;
          this.EnemyDy = player.playerY - this.EnemyY ;
          this.EnemyAngle = Math.atan2(this.EnemyDy,this.EnemyDx) ;
          this.EnemyVx = Math.cos(this.EnemyAngle) * this.EnemySpeed ;
          this.EnemyVy = Math.sin(this.EnemyAngle) * this.EnemySpeed ;
+        }
          this.EnemyX += this.EnemyVx ;
          this.EnemyY += this.EnemyVy ;
          const margin = this.EnemySize * 2 ;
         if(this.EnemyY < -margin || 
         this.EnemyY > canvas.height + margin || 
         this.EnemyX < -margin || 
-        this.EnemyX > canvas.width + margin){
+        this.EnemyX > canvas.width + margin||
+        this.EnemyHP <= 0){
         this.active = false
+        }
+if(this.type === 'boss') {
+    if(currentTime - this.lastShotTime > 20){
+        this.shot(20);
+        this.lastShotTime = currentTime ;
     }
+    bossAngle += Math.log(this.foo + 1) * 0.5; 
+    this.foo += 3; 
+}
 }
     draw (){
         ctx.save();
@@ -192,33 +231,51 @@ class Enemy {
             case 'toucan' :
                 drawImg((Math.floor(frameCounter / 10) % 2) + 19, -this.EnemySize / 2, -this.EnemySize / 2, this.EnemySize); 
                 break;
+            case 'boss'   :
+                drawImg((Math.floor(frameCounter / 10) % 2) + 2, -this.EnemySize / 2, -this.EnemySize / 2, this.EnemySize); 
+                break;
+            case 'shot'   :
+                drawImg((Math.floor(frameCounter / 10) % 2) + 16, -this.EnemySize / 2, -this.EnemySize / 2, this.EnemySize);
+                break;
          }
 
          ctx.restore();
              }
+    shot(size){
+        Enemies.push(new Enemy(this.EnemyX + this.EnemySize /2,this.EnemyY + this.EnemySize /2,20,'shot',size)) ;
+    }
 }
-function EnemyMove(){
+function EnemyMove(currentTime){
     for(let i = 0 ; i < Enemies.length ; i++){
-        Enemies[i].move() ;
+        Enemies[i].move(currentTime) ;
         Enemies[i].draw() ;
     }
     Enemies = Enemies.filter(Enemy => Enemy.active);
     }
 function EnemySpawn(currentTime){
     if(currentTime - lastSpawnTime > EnemySpawnQueue){
-        const random = Math.floor(Math.random()*4) ;
-        switch (random){
-        case 0 : Enemies.push(new Enemy(Math.random()*canvas.width,0,EnemySpeed,20,EnemyType[Math.floor(Math.random()*3)],EnemySize)) ;
-        break;
-        case 1 : Enemies.push(new Enemy(Math.random()*canvas.width,canvas.height,EnemySpeed,20,EnemyType[Math.floor(Math.random()*3)],EnemySize)) ;
-        break;
-        case 2 : Enemies.push(new Enemy(0,Math.random()*canvas.height,EnemySpeed,20,EnemyType[Math.floor(Math.random()*3)],EnemySize)) ;
-        break;
-        case 3 : Enemies.push(new Enemy(canvas.width,Math.random()*canvas.height,EnemySpeed,20,EnemyType[Math.floor(Math.random()*3)],EnemySize)) ;
-        }
+        spawn(EnemyType[Math.floor(Math.random()*3)]);
         lastSpawnTime = currentTime ;
     }
 }
+function spawn (type){
+    const random = Math.floor(Math.random()*4) ;
+switch (random){
+    case 0 : Enemies.push(new Enemy(Math.random()*canvas.width,0,20,type,EnemySize)) ;
+        break;
+    case 1 : Enemies.push(new Enemy(Math.random()*canvas.width,canvas.height,20,type,EnemySize)) ;
+        break;
+    case 2 : Enemies.push(new Enemy(0,Math.random()*canvas.height,20,type,EnemySize)) ;
+        break;
+    case 3 : Enemies.push(new Enemy(canvas.width,Math.random()*canvas.height,20,type,EnemySize)) ;
+        break;
+    }
+    }    
+
+function boss (){
+    spawn('boss')
+}
+
 function collision(object1,object2,Size1,Size2,x1,x2,y1,y2){
     for (let i = object1.length -1; i>=0; i--){
         let p = object1[i];
@@ -240,12 +297,16 @@ function collision(object1,object2,Size1,Size2,x1,x2,y1,y2){
 
 function playerEnemyHit(currentTime){
 const result = collision([player],Enemies,'playerSize','EnemySize','playerX','EnemyX','playerY','EnemyY');
-if(result !== null && player.playerInvincible === false)
+if(result !== null && result[1].type === 'shot'){
+    player.playerHP -= 0.1 ;
+    result[1].active = false;
+}else if(result !== null && player.playerInvincible === false)
 {
 player.playerHP -= result[1].EnemyPower ;
-
 player.lastInvincivleTime = currentTime ;
 player.playerInvincible = true ;
+
+
 }
 }
 
@@ -255,17 +316,26 @@ function pShotEnemyHit() {
         for (let j = Enemies.length - 1; j >= 0; j--) {
             let e = Enemies[j];
             if (
-                p.pShotX < e.EnemyX + EnemySize &&
+                p.pShotX < e.EnemyX + e.EnemySize/1.5 &&
                 p.pShotX + pShotSize > e.EnemyX &&
-                p.pShotY < e.EnemyY + EnemySize &&
+                p.pShotY < e.EnemyY + e.EnemySize/1.5 &&
                 p.pShotY + pShotSize > e.EnemyY
             ) {
+               if(e.type === 'boss'){
+                    p.active = false;
+                    e.EnemyHP -= 1 ;
+                    console.log(e.EnemyHP);
+                    break;
+               }
+                else{
                 p.active = false;
-                e.active = false;                
-                break; 
+                e.active = false;
+                break;                
+                } 
             }
         }
     }
     pShots = pShots.filter(pShot => pShot.active);
     Enemies = Enemies.filter(enemy => enemy.active);
 }
+boss();
